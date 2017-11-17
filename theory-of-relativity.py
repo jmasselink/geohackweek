ae51b16d937f6fca48c8f8c6f3ac517819eafc8f
Joel M <jmasselink@gmail.com>
2017-11-16 21:49:22 -0800

    added a few random jupyter notebooks floating around

---
+++ theory-of-relativity.py
@@ -0,0 +1,11 @@
+#theory of relativity - an introduction
+
+E = mc$$2
+import numpy as np ## linear algebra library
+import matplotlib.pyplot as plt
+c = np.linspace(0., 1., 100)
+m = 1
+e = m * c **2
+E
+%matplotlib inline
+plt.plot(c,E)
